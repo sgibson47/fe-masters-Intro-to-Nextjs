9a77dbab52213eccf38f48f8c884a5d23a225b52
Sam Gibson <sam.gibson@vividseats.com>
2020-10-05 14:10:40 -0500

    end of routing section

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -5,12 +5,19 @@ import Link from 'next/link'
 const Page = () => {
     const router = useRouter();
 
+    const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note ${i}`}) );
+
     return (
         <div>
             <h1>Notes Page</h1>
-            <Link href='/notes/[id]' as={'/notes/1'}>
-                <a>Notes 1</a>
-            </Link>
+
+            {notes.map(note => (
+                <div>
+                    <Link key={note.id} href='/notes/[id]' as={`/notes/${note.id}`}>
+                        <a>{note.title}</a>
+                    </Link>
+                </div>
+            ))}
         </div>
     )
 }
