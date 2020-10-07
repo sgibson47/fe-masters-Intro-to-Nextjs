aaa4a0133e6d8cd24a46a52bb7fc27b4f4b07123
Sam Gibson <sam.gibson@vividseats.com>
2020-10-07 14:24:44 -0500

    reorganize files; add global styles; add styles via css modules

--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import styles from '../../styles/myFirstModule.module.css'
 
 const Page = () => {
     const router = useRouter();
@@ -8,13 +9,13 @@ const Page = () => {
     const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note ${i}`}) );
 
     return (
-        <div>
+        <div >
             <h1>Notes Page</h1>
 
             {notes.map(note => (
-                <div>
-                    <Link key={note.id} href='/notes/[id]' as={`/notes/${note.id}`}>
-                        <a>{note.title}</a>
+                <div key={note.id}>
+                    <Link key={note.id} href='/notes/[id]' as={`/notes/${note.id}`} > 
+                        <a className={styles.link}>{note.title}</a>
                     </Link>
                 </div>
             ))}
