ce7ed57cfcfadea71350b2cea46b783a31061d90
Sam Gibson <sam.gibson@vividseats.com>
2020-10-13 16:50:14 -0500

    fix use of withPlugins

---
+++ next-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+
+// A file named next-env.d.ts will be created in the root of your project. 
+// This file ensures Next.js types are picked up by the TypeScript compiler. 
+// You cannot remove it, however, you can edit it (but you don't need to).
+
+// TypeScript strict mode is turned off by default. 
+// When you feel comfortable with TypeScript, 
+// it's recommended to turn it on in your tsconfig.json.
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 // this config file doesn't get complied by Babel, so we can't use import statements
 // use require() instead 
-const { PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+const {
+    PHASE_PRODUCTION_BUILD,
+    PHASE_PRODUCTION_SERVER,
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_EXPORT,
+  } = require('next/constants');
+//     // phase is the current context in which the configuration is loaded
+//     // There are 4 Phases:
+//     // PHASE_EXPORT -- 
+//     // PHASE_PRODUCTION_BUILD -- 
+//     // PHASE_PRODUCTION_SERVER -- when it's live running on the prod server
+//     // PHASE_DEVELOPMENT_SERVER -- when you're developing 
 
 // Plugins!
 // generally named with____
@@ -32,45 +43,51 @@ dotenvLoad()
 // creates the plugin with the now available environemtn variables injected into it
 const withNextEnv = nextEnv()
 
-// But what if I want to use multiple plugins? use withPlugins
+// But what if I want to use nultiple plugins? use withPlugins
 const withPlugins = require('next-compose-plugins')
-const withSass = require('@zeit/next-sass')
 // then wrap the dcongif object you return in withPlugins like this:
 // module.exports = withPlugins([
-//     withSass,
-//     withNextEnv
+//     withOffline,
+//     withReactSvg, {
+//       // config for reactSvgPlugin
+//     },
+//    withNextEnv
 //   ], config)
 // it takes in 2 args -- 1) an array of plugins, 2) a config object
 
+const sass = require('@zeit/next-sass');
 
 
-module.exports = (phase, {defaultConfig}) => {
-    // phase is the current context in which the configuration is loaded
-    // There are 4 Phases:
-    // PHASE_EXPORT -- 
-    // PHASE_PRODUCTION_BUILD -- 
-    // PHASE_PRODUCTION_SERVER -- when it's live running on the prod server
-    // PHASE_DEVELOPMENT_SERVER -- when you're developing 
 
-    // this code will run when Next is running a dev server, i.e. when you run yarn dev
-    if(phase === PHASE_DEVELOPMENT_SERVER){
-        console.log("I'm in dev mode");
+// module.exports = (phase, {defaultConfig}) => {
+//     // this code will run when Next is running a dev server, i.e. when you run yarn dev
+//     if(phase === PHASE_DEVELOPMENT_SERVER){
+//         console.log("I'm in dev mode");
 
-        // If you want a different config for this phase, 
-        // you'd return your altered config object here
-        return withPlugins(
-            [ withNextEnv, withSass ],
-            {...defaultConfig,
-            // spread the default
-            // then add any modifications you'd like
-            // Scott encourages us to not modify arguments in JS in general
-            }
-        )
-    }
+//         // If you want a different config for this phase, 
+//         // you'd return your altered config object here
+//         return withNextEnv({
+//             // spread the default
+//             ...defaultConfig,
+//             // then add any modifications you'd like
+//             // e.g. 
+//             // webpack: {
+//             //     plugins: [new BundleAnalyzerPlugin()]
+//             //   }
+//             // then next will merge your stuff
+//             // Scott encourages us to not modify arguments in JS in general
+//         });
+//     }
+//     return withNextEnv(defaultConfig);
+// }
 
-    return withPlugins(
-        [ withNextEnv, withSass ],
-        {...defaultConfig}
-    );
-}
+module.exports = withPlugins([
+    [sass, {
+      cssModules: true,
+    }, [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]],
+    withNextEnv
+  ]
+  );
+
+
 
